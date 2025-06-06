4423e2c6deb1d4d09d84cf9b7a3d4ae381d5ce7f
rubenmt.dev <rubenmt.dev@gmail.com>
2025-06-06 14:27:56 +0200

    final

---
+++ documentacion/Recursos instalacion/openjfx-21.0.6_windows-x64_bin-sdk/javafx-sdk-21.0.6/clases_generadas/asd.java
@@ -0,0 +1,16 @@
+public class asd {
+
+    private String asd = "asd";
+
+    public void asd(asd asd) {
+        asd
+        return asd;
+        }
+    }
+
+    public String ddd(ddd ddd) {
+        ddd
+        return ddd;
+    }
+
+}
---
+++ documentacion/Recursos instalacion/openjfx-21.0.6_windows-x64_bin-sdk/javafx-sdk-21.0.6/funciones_generadas/BufferedReader.read.java
@@ -0,0 +1,2 @@
+public void BufferedReader.read() {
+}
